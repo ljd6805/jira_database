ffc7fe51516ec9c2e66cb36fa85433d0714d3708
ljd6805 <75003656+ljd6805@users.noreply.github.com>
2026-08-04 21:00:09 +0900

    feat: export comment and shared store APIs

--- src/jira_collector/exporter/__init__.py
+++ src/jira_collector/exporter/__init__.py
@@ -1,6 +1,18 @@
 """Jira parser 결과를 분석용 파일로 저장하는 exporter 도구 모음입니다."""
 
+from .comment_jsonl_exporter import CommentJsonlExporter
 from .issue_jsonl_exporter import IssueJsonlExporter
-from .models import IssueExportResult
+from .models import CommentExportResult, IssueExportResult
+from .run_summary_store import RunSummaryError, RunSummaryStore
+from .run_warning_store import RunWarningStore, RunWarningStoreError
 
-__all__ = ["IssueExportResult", "IssueJsonlExporter"]
+__all__ = [
+    "CommentExportResult",
+    "CommentJsonlExporter",
+    "IssueExportResult",
+    "IssueJsonlExporter",
+    "RunSummaryError",
+    "RunSummaryStore",
+    "RunWarningStore",
+    "RunWarningStoreError",
+]
