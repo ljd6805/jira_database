1d61041c3e62b6fabc04ae5d9069b37b48e8c05f
ljd6805 <75003656+ljd6805@users.noreply.github.com>
2026-08-07 16:39:22 +0900

    feat: add knowledge input package

---
+++ src/jira_collector/knowledge_input/__init__.py
@@ -0,0 +1,13 @@
+"""ANALYSIS 데이터를 OpenCode 분석용 이슈 단위 입력 패키지로 조립하는 도구입니다."""
+
+from .builder import (
+    IssueKnowledgeInputBuilder,
+    KnowledgeInputBuildError,
+    KnowledgeInputBuildResult,
+)
+
+__all__ = [
+    "IssueKnowledgeInputBuilder",
+    "KnowledgeInputBuildError",
+    "KnowledgeInputBuildResult",
+]
